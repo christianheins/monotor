f7145207a4bb9ae89bbdb0f99260d02f641b4d04
Christian Heins <129910735+christianheins@users.noreply.github.com>
2024-07-03 23:23:03 +0200

    sad

--- main_monotor.py
+++ main_monotor.py
@@ -130,7 +130,16 @@ def main():
     @st.experimental_fragment
     def return_soundcloudembed():
         html = """
-        <iframe width="100%" height="220" scrolling="yes" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1671680115&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/tellel" title="Tellel" target="_blank" style="color: #cccccc; text-decoration: none;">Tellel</a> · <a href="https://soundcloud.com/tellel/turu-anasi-bamboo" title="Turu Anasi - Bamboo" target="_blank" style="color: #cccccc; text-decoration: none;">Turu Anasi - Bamboo</a></div>
+        <iframe 
+            width="100%"
+            height="220" 
+            scrolling="yes" 
+            frameborder="no" 
+            allow="autoplay" 
+            src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1671680115&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+            >
+        </iframe>
+        <div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/tellel" title="Tellel" target="_blank" style="color: #cccccc; text-decoration: none;">Tellel</a> · <a href="https://soundcloud.com/tellel/turu-anasi-bamboo" title="Turu Anasi - Bamboo" target="_blank" style="color: #cccccc; text-decoration: none;">Turu Anasi - Bamboo</a></div>
         """
         components.html(html, height=250)
         #st.divider()
