760ee73fe428e48244c08d1843173ca0ef2c59e8
Christian Heins <129910735+christianheins@users.noreply.github.com>
2024-07-04 19:09:13 +0200

    dsa

--- main_monotor.py
+++ main_monotor.py
@@ -310,7 +310,12 @@ def main():
                     <img src="https://github.com/christianheins/monotor/blob/main/images/46022236_270547913605848_7412623757248725390_n.jpg?raw=true" alt="Artist 1" class="artist-image">
                     <div class="artist-info">
                         <h2 class="artist-name">Mono Heins</h2>
-                        <p class="artist-description">Deep House, Tech House, Progressive House, Minimal, Tropical And Afrobeats. \n\n I range all the tempos, from low beats to high highs...</p>
+                        <p class="artist-description">
+                        
+                        Deep House, Tech House, Progressive House, Minimal, Tropical And Afrobeats. 
+                        <br> 
+                        I range all the tempos, from low beats to high highs...</p>
+                    
                     </div>
                 </div>
 
