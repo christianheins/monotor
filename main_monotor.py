4ff31b817e6bdfc0cef81e8a481f978723e89161
Christian Heins <129910735+christianheins@users.noreply.github.com>
2024-07-04 19:41:11 +0200

    asdasd

--- main_monotor.py
+++ main_monotor.py
@@ -256,7 +256,7 @@ def main():
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title style='color: black;'>Artists Showcase</title>
+            <title style='color: white;'>Artists Showcase</title>
             <style>
                 body {
                     font-family: 'Arial', sans-serif;
