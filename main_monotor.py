05e8c3d4b371d2aeeff1f94a267e69df51a6f8b3
Christian Heins <129910735+christianheins@users.noreply.github.com>
2024-07-04 19:54:28 +0200

    sda

--- main_monotor.py
+++ main_monotor.py
@@ -276,7 +276,7 @@ def main():
                     border-radius: 10px;
                     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
                     overflow: hidden;
-                    width: 300px;
+                    width: 400px;
                     transition: transform 0.2s;
                 }
 
