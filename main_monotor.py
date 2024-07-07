2915b642fd5404fdd146c124e36da9a049423d7b
Christian Heins <129910735+christianheins@users.noreply.github.com>
2024-07-07 22:39:26 +0200

    dsa

--- main_monotor.py
+++ main_monotor.py
@@ -189,17 +189,20 @@ def main():
 
     // When the user clicks the button, open the modal 
     btn.onclick = function() {
-    modal.style.display = "block";
+        console.log("Modal test")
+        modal.style.display = "block";
     }
 
     // When the user clicks on <span> (x), close the modal
     span.onclick = function() {
-    modal.style.display = "none";
+        console.log("Modal test")
+        modal.style.display = "none";
     }
 
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
     if (event.target == modal) {
+        console.log("Modal test")
         modal.style.display = "none";
     }
     }
